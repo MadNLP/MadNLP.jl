11393ffc5f08a5d4eaab517f39ee6a54abfffaaa
Sungho Shin <sungho.shin@wisc.edu>
2020-10-01 18:29:38 -0500

    minor edits

--- src/MadNLP.jl
+++ src/MadNLP.jl
@@ -4,6 +4,7 @@
 module MadNLP
 
 import Pkg.Artifacts: @artifact_str
+import Pkg.TOML: parsefile
 import IterativeSolvers, MathOptInterface, MPI
 import Libdl: dlopen, dlext, RTLD_DEEPBIND, RTLD_GLOBAL
 import Metis: partition
@@ -30,19 +31,19 @@ const MOIU = MathOptInterface.Utilities
 export madnlp
 
 # Version info
-version() = v"0.1.0"
-introduce() = "MadNLP version $(version())"
+version() = parsefile(joinpath(@__DIR__,"..","Project.toml"))["version"]
+introduce() = "MadNLP version v$(version())"
 
 # Linear solver dependencies
-include("../deps/deps.jl")
+include(joinpath("..","deps","deps.jl"))
 include("enums.jl")
 include("utils.jl")
 include("nonlinearprogram.jl")
 include("matrixtools.jl")
 include("graphtools.jl")
-include("LinearSolvers/linearsolvers.jl")
-include("interiorpointsolver.jl")
-include("Interfaces/interfaces.jl")
+include(joinpath("LinearSolvers","linearsolvers.jl"))
+include(joinpath("interiorpointsolver.jl"))
+include(joinpath("Interfaces","interfaces.jl"))
 
 # Initialize
 function __init__()
@@ -50,13 +51,15 @@ function __init__()
     @isdefined(libmumps) && dlopen(libmumps,RTLD_DEEPBIND)
     @isdefined(libhsl) && dlopen(libhsl,RTLD_DEEPBIND)
     @isdefined(libpardiso) && dlopen(libpardiso,RTLD_DEEPBIND)
-    @isdefined(libmkl32) && dlopen.(joinpath.(artifact"MKL",[
-        "lib/libmkl_core.$(dlext)",
-        "lib/libmkl_sequential.$(dlext)",
-        "lib/libmkl_intel_lp64.$(dlext)"]),RTLD_GLOBAL)
+    @isdefined(libmkl32) && dlopen.(joinpath.(artifact"MKL","lib",[
+        "libmkl_core.$(dlext)",
+        "libmkl_sequential.$(dlext)",
+        "libmkl_intel_lp64.$(dlext)"]),RTLD_GLOBAL)
     @isdefined(libopenblas32) && dlopen(libopenblas32,RTLD_GLOBAL)
     set_blas_num_threads(haskey(ENV,"JULIA_NUM_THREADS") ? parse(Int,ENV["JULIA_NUM_THREADS"]) : 1 ;permanent=true)
 end
 
 end # end module
+
+
 
