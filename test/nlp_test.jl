d534e5d25776d5aa70ade5a6ad0c48907dc85051
Sungho Shin <sungho.shin@wisc.edu>
2020-12-16 00:22:37 -0600

    comment out part of nlp test

--- test/nlp_test.jl
+++ test/nlp_test.jl
@@ -75,109 +75,109 @@ sets = [
     ],
     [
         ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.PardisoCPU,
+            linear_solver=MadNLP.PardisoMKL,
             print_level=MadNLP.ERROR),
         [],
-        isdefined(MadNLP,:PardisoCPU)
+        isdefined(MadNLP,:PardisoMKL)
     ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackCPU,
-            lapackcpu_algorithm=MadNLP.LapackCPU.BUNCHKAUFMAN,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackCPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackCPU,
-            lapackcpu_algorithm=MadNLP.LapackCPU.LU,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackCPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackCPU,
-            lapackcpu_algorithm=MadNLP.LapackCPU.QR,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackCPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackGPU,
-            lapackgpu_algorithm=MadNLP.LapackGPU.BUNCHKAUFMAN,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackGPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackGPU,
-            lapackgpu_algorithm=MadNLP.LapackGPU.LU,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackGPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_solver=MadNLP.LapackGPU,
-            lapackgpu_algorithm=MadNLP.LapackGPU.QR,
-            print_level=MadNLP.ERROR),
-        [],
-        isdefined(MadNLP,:LapackGPU)
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            fixed_variable_treatment=MadNLP.RELAX_BOUND,
-            print_level=MadNLP.ERROR),
-        [],
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            reduced_system=false,
-            print_level=MadNLP.ERROR),
-        ["infeasible","eigmina"], # numerical errors
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            inertia_correction_method=MadNLP.INERTIA_FREE,
-            reduced_system=false),
-        ["infeasible","eigmina"], # numerical errors
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            inertia_correction_method=MadNLP.INERTIA_FREE,
-            print_level=MadNLP.ERROR),
-        [],
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            iterator=MadNLP.Krylov,
-            print_level=MadNLP.ERROR),
-        ["unbounded"],
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            linear_system_scaler=isdefined(MadNLP,:Mc19) ? MadNLP.Mc19 : MadNLP.DummyModule,
-            print_level=MadNLP.ERROR),
-        ["eigmina"],
-        true
-    ],
-    [
-        ()->MadNLP.Optimizer(
-            disable_garbage_collector=true,
-            output_file=".test.out"
-        ),
-        ["infeasible","unbounded","eigmina"], # just checking logger; no need to test all
-        isdefined(MadNLP,:Umfpack )
-    ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackCPU,
+    #         lapackcpu_algorithm=MadNLP.LapackCPU.BUNCHKAUFMAN,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackCPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackCPU,
+    #         lapackcpu_algorithm=MadNLP.LapackCPU.LU,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackCPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackCPU,
+    #         lapackcpu_algorithm=MadNLP.LapackCPU.QR,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackCPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackGPU,
+    #         lapackgpu_algorithm=MadNLP.LapackGPU.BUNCHKAUFMAN,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackGPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackGPU,
+    #         lapackgpu_algorithm=MadNLP.LapackGPU.LU,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackGPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_solver=MadNLP.LapackGPU,
+    #         lapackgpu_algorithm=MadNLP.LapackGPU.QR,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     isdefined(MadNLP,:LapackGPU)
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         fixed_variable_treatment=MadNLP.RELAX_BOUND,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         reduced_system=false,
+    #         print_level=MadNLP.ERROR),
+    #     ["infeasible","eigmina"], # numerical errors
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         inertia_correction_method=MadNLP.INERTIA_FREE,
+    #         reduced_system=false),
+    #     ["infeasible","eigmina"], # numerical errors
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         inertia_correction_method=MadNLP.INERTIA_FREE,
+    #         print_level=MadNLP.ERROR),
+    #     [],
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         iterator=MadNLP.Krylov,
+    #         print_level=MadNLP.ERROR),
+    #     ["unbounded"],
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         linear_system_scaler=isdefined(MadNLP,:Mc19) ? MadNLP.Mc19 : MadNLP.DummyModule,
+    #         print_level=MadNLP.ERROR),
+    #     ["eigmina"],
+    #     true
+    # ],
+    # [
+    #     ()->MadNLP.Optimizer(
+    #         disable_garbage_collector=true,
+    #         output_file=".test.out"
+    #     ),
+    #     ["infeasible","unbounded","eigmina"], # just checking logger; no need to test all
+    #     isdefined(MadNLP,:Umfpack )
+    # ],
 ]
 
 @testset "NLP test" for (optimizer_constructor,exclude,availability) in sets
