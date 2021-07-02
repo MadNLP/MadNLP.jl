c794288d3dbd0d0c67a39d7197cf392dd172f97f
Sungho Shin <sshin@anl.gov>
2021-07-02 10:12:42 -0500

    changes for registering extension packages

--- benchmark/runbenchmark.jl
+++
@@ -1,8 +0,0 @@
-using Plots
-
-"current" in ARGS && run(`julia --project=. -p 2 benchmarks.jl current`)
-"master" in ARGS && run(`julia --project=. -p 2 benchmarks.jl master`)
-"ipopt" in ARGS && run(`julia --project=. -p 2 benchmarks.jl ipopt`)
-
-
-
