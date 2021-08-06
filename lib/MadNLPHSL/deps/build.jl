f05bd6ee81915004ab107656fc8ba3963a4d685b
Sungho Shin <sshin@anl.gov>
2021-08-06 11:26:29 -0500

    hsl build with mkl (#54)

--- lib/MadNLPHSL/deps/build.jl
+++ lib/MadNLPHSL/deps/build.jl
@@ -3,6 +3,12 @@
 
 using Pkg.Artifacts, BinaryProvider
 
+ if haskey(ENV,"MADNLP_HSL_BLAS")
+    blasvendor = (ENV["MADNLP_HSL_BLAS"]=="openblas") ? :openblas : :mkl
+else
+    blasvendor =  artifact_hash("MKL",joinpath(@__DIR__, "..", "Artifacts.toml")) != nothing ? :mkl : :openblas
+ end
+
 const verbose = "--verbose" in ARGS
 const prefix = Prefix(@__DIR__)
 const so = BinaryProvider.platform_dlext()
@@ -16,8 +22,9 @@ const hsl_version = haskey(ENV,"MADNLP_HSL_VERSION_NUMBER") ? ENV["MADNLP_HSL_VE
 const FC = haskey(ENV,"MADNLP_FC") ? ENV["MADNLP_FC"] : `gfortran`
 const libmetis_dir = joinpath(artifact"METIS", "lib")
 const with_metis = `-L$libmetis_dir $rpath$libmetis_dir -lmetis`
-const libopenblas_dir = joinpath(artifact"OpenBLAS32","lib")
-const with_openblas = `-L$libopenblas_dir $rpath$libopenblas_dir -lopenblas`
+const libblas_dir = joinpath(blasvendor == :mkl ? artifact"MKL" : artifact"OpenBLAS32","lib")
+const with_blas = blasvendor == :mkl ? `-L$libblas_dir $rpath$libblas_dir -lmkl_rt` :
+    `-L$libblas_dir $rpath$libblas_dir -lopenblas`
 const installer = Sys.isapple() ? "brew install" : Sys.iswindows() ? "pacman -S" : "sudo apt install"
 
 rm(libdir;recursive=true,force=true)
@@ -45,7 +52,7 @@ if hsl_source_path != ""
         push!(OC,OutputCollector(`$FC -fopenmp -fPIC -c -O3 -o hsl_mc68/C/hsl_mc68i_ciface.o hsl_mc68/C/hsl_mc68i_ciface.f90`,verbose=verbose))
         push!(OC,OutputCollector(`$FC -fopenmp -fPIC -c -O3 -o hsl_ma97/C/hsl_ma97d_ciface.o hsl_ma97/C/hsl_ma97d_ciface.f90`,verbose=verbose))
         wait.(OC); empty!(OC)
-        wait(OutputCollector(`$FC -o$(libdir)/libhsl.$so -shared -fPIC -O3 common/deps90.o common/deps.o mc19/mc19d.o ma27/ma27d.o ma57/ma57d.o hsl_ma77/hsl_ma77d.o hsl_ma77/C/hsl_ma77d_ciface.o hsl_ma86/hsl_ma86d.o hsl_ma86/C/hsl_ma86d_ciface.o hsl_mc68/C/hsl_mc68i_ciface.o hsl_ma97/hsl_ma97d.o hsl_ma97/C/hsl_ma97d_ciface.o -fopenmp $with_metis $with_openblas`,verbose=verbose))
+        wait(OutputCollector(`$FC -o$(libdir)/libhsl.$so -shared -fPIC -O3 common/deps90.o common/deps.o mc19/mc19d.o ma27/ma27d.o ma57/ma57d.o hsl_ma77/hsl_ma77d.o hsl_ma77/C/hsl_ma77d_ciface.o hsl_ma86/hsl_ma86d.o hsl_ma86/C/hsl_ma86d_ciface.o hsl_mc68/C/hsl_mc68i_ciface.o hsl_ma97/hsl_ma97d.o hsl_ma97/C/hsl_ma97d_ciface.o -fopenmp $with_metis $with_blas`,verbose=verbose))
         cd("$(@__DIR__)")
         product = FileProduct(prefix,joinpath(libdir,"libhsl.$so"), :libhsl)
     end
