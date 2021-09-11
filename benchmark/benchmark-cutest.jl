2b29ed8a7abb419c13df7c4579de815a49fc329d
Sungho Shin <sshin@anl.gov>
2021-09-11 15:40:26 -0500

    benchmark update (#76)

--- benchmark/benchmark-cutest.jl
+++ benchmark/benchmark-cutest.jl
@@ -1,24 +1,36 @@
 include("config.jl")
 Pkg.add(PackageSpec(name="CUTEst",rev="main")) # will be removed once the new CUTEst version is released
 
-function get_status(code::Symbol)
-    if code == :first_order
-        return 1
-    elseif code == :acceptable
-        return 2
-    else
-        return 3
-    end
-end
-
 @everywhere using CUTEst
 
 if SOLVER == "master" || SOLVER == "current"
-    @everywhere using MadNLP, MadNLPHSL
-    @everywhere solver = nlp -> madnlp(nlp,linear_solver=MadNLPMa57,max_wall_time=900.,tol=1e-6, print_level=PRINT_LEVEL)
+    @everywhere begin
+        using MadNLP, MadNLPHSL
+        solver = nlp -> madnlp(nlp,linear_solver=MadNLPMa57,max_wall_time=900.,tol=1e-6, print_level=PRINT_LEVEL)
+        function get_status(code::MadNLP.Status)
+            if code == MadNLP.SOLVE_SUCCEEDED
+                return 1
+            elseif code == MadNLP.SOLVED_TO_ACCEPTABLE_LEVEL
+                return 2
+            else
+                return 3
+            end
+        end
+    end
 elseif SOLVER == "ipopt"
-    @everywhere solver = nlp -> ipopt(nlp,linear_solver="ma57",max_cpu_time=900.,tol=1e-6, print_level=PRINT_LEVEL)
-    @everywhere using NLPModelsIpopt
+    @everywhere begin
+        solver = nlp -> ipopt(nlp,linear_solver="ma57",max_cpu_time=900.,tol=1e-6, print_level=PRINT_LEVEL)
+        using NLPModelsIpopt
+        function get_status(code::Symbol)
+            if code == :first_order
+                return 1
+            elseif code == :acceptable
+                return 2
+            else
+                return 3
+            end
+        end
+    end
 elseif SOLVER == "knitro"
     # TODO
 else
@@ -42,10 +54,8 @@ end
             end
         end
         gcoff && GC.enable(true);
-        retval.elapsed_time = t
-        retval.solver_specific[:mem] = mem
         finalize(nlp)
-        return retval
+        return (status=get_status(retval.status),time=t,mem=mem,iter=retval.iter)
     catch e
         finalize(nlp)
         throw(e)
@@ -62,9 +72,9 @@ function benchmark(solver,probs;warm_up_probs = [])
 
     println("Solving problems")
     retvals = pmap(prob->evalmodel(prob,solver),probs)
-    status = [get_status(retval.status) for retval in retvals]
-    time   = [retval.elapsed_time for retval in retvals]
-    mem    = [retval.solver_specific[:mem] for retval in retvals]
+    status = [retval.status for retval in retvals]
+    time   = [retval.time for retval in retvals]
+    mem    = [retval.mem for retval in retvals]
     iter   = [retval.iter for retval in retvals]
     status,time,mem,iter
 end
