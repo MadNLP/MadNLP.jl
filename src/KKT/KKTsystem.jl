d70257199c5b4bb293c184bbf7a048a881073aef
fpacaud <francoispacaud8@gmail.com>
2021-10-20 23:40:13 +0200

    add documentation for KKT systems

---
+++ src/KKT/KKTsystem.jl
@@ -0,0 +1,247 @@
+
+abstract type AbstractKKTSystem{T, MT<:AbstractMatrix{T}} end
+
+"""
+    AbstractUnreducedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT}
+
+Augmented KKT system associated to the linearization of the KKT
+conditions at the current primal-dual iterate ``(x, s, y, z, ν, w)``.
+
+The associated matrix is
+```
+[Wₓₓ  0  Aₑ'  Aᵢ'  -I   0 ]  [Δx]
+[ 0   0   0   -I    0  -I ]  [Δs]
+[Aₑ   0   0    0    0   0 ]  [Δy]
+[Aᵢ  -I   0    0    0   0 ]  [Δz]
+[V    0   0    0    X   0 ]  [Δν]
+[0    W   0    0    0   S ]  [Δw]
+```
+with
+* ``Wₓₓ``: Hessian of the Lagrangian.
+* ``Aₑ``: Jacobian of the equality constraints
+* ``Aᵢ``: Jacobian of the inequality constraints
+* ``X = diag(x)``
+* ``S = diag(s)``
+* ``V = diag(ν)``
+* ``W = diag(w)``
+"""
+abstract type AbstractUnreducedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT} end
+
+"""
+    AbstractReducedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT}
+
+The reduced KKT system is a simplification of the original Augmented KKT system.
+Comparing to [`AbstractUnreducedKKTSystem`](@ref)), `AbstractReducedKKTSystem` removes
+the two last rows associated to the bounds' duals ``(ν, w)``.
+
+At a primal-dual iterate ``(x, s, y, z)``, the matrix writes
+```
+[Wₓₓ + Σₓ   0    Aₑ'   Aᵢ']  [Δx]
+[ 0         Σₛ    0    -I ]  [Δs]
+[Aₑ         0     0     0 ]  [Δy]
+[Aᵢ        -I     0     0 ]  [Δz]
+```
+with
+* ``Wₓₓ``: Hessian of the Lagrangian.
+* ``Aₑ``: Jacobian of the equality constraints
+* ``Aᵢ``: Jacobian of the inequality constraints
+* ``Σₓ = X⁻¹ V``
+* ``Σₛ = S⁻¹ W``
+
+"""
+abstract type AbstractReducedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT} end
+
+"""
+    AbstractCondensedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT}
+
+The condensed KKT system simplifies further the [``AbstractReducedKKTSystem`](@ref)
+by removing the rows associated to the slack variables ``s`` and the inequalities.
+
+At the primal-dual iterate ``(x, y)``, the matrix writes
+```
+[Wₓₓ + Σₓ + Aᵢ' Σₛ Aᵢ    Aₑ']  [Δx]
+[         Aₑ              0 ]  [Δy]
+```
+with
+* ``Wₓₓ``: Hessian of the Lagrangian.
+* ``Aₑ``: Jacobian of the equality constraints
+* ``Aᵢ``: Jacobian of the inequality constraints
+* ``Σₓ = X⁻¹ V``
+* ``Σₛ = S⁻¹ W``
+"""
+abstract type AbstractCondensedKKTSystem{T, MT} <: AbstractKKTSystem{T, MT} end
+
+#=
+    Templates
+=#
+
+"Number of primal variables associated to the KKT system."
+function num_variables end
+
+"""
+    get_kkt(kkt::AbstractKKTSystem)::AbstractMatrix
+
+Return a pointer to the KKT matrix implemented in `kkt`.
+The pointer is passed afterward to a linear solver.
+"""
+function get_kkt end
+
+"Get Jacobian matrix"
+function get_jacobian end
+
+"Get Hessian matrix"
+function get_hessian end
+
+"""
+    initialize!(kkt::AbstractKKTSystem)
+
+Initialize KKT system with default values.
+Called when we initialize the `InteriorPointSolver` storing the current KKT system `kkt`.
+"""
+function initialize! end
+
+"""
+    build_kkt!(kkt::AbstractKKTSystem)
+
+Assemble the KKT matrix before calling the factorization routine.
+
+"""
+function build_kkt! end
+
+"""
+    compress_hessian!(kkt::AbstractKKTSystem)
+
+Compress the Hessian inside `kkt`'s internals.
+This function is called every time a new Hessian is evaluated.
+
+Default implementation do nothing.
+
+"""
+function compress_hessian! end
+
+"""
+    compress_jacobian!(kkt::AbstractKKTSystem)
+
+Compress the Jacobian inside `kkt`'s internals.
+This function is called every time a new Jacobian is evaluated.
+
+By default, the function updates in the Jacobian the coefficients
+associated to the slack variables.
+
+"""
+function compress_jacobian! end
+
+"""
+    jtprod!(y::AbstractVector, kkt::AbstractKKTSystem, x::AbstractVector)
+
+Multiply with transpose of Jacobian and store the result
+in `y`, such that ``y = A' x`` (with ``A`` current Jacobian).
+"""
+function jtprod! end
+
+"""
+    regularize_diagonal!(kkt::AbstractKKTSystem, primal_values::AbstractVector, dual_values::AbstractVector)
+
+Regularize the values in the diagonal of the KKT system.
+Called internally inside the interior-point routine.
+"""
+function regularize_diagonal! end
+
+"""
+    set_jacobian_scaling!(kkt::AbstractKKTSystem, scaling::AbstractVector)
+
+Set the scaling of the Jacobian with the vector `scaling` storing
+the scaling for all the constraints in the problem.
+
+"""
+function set_jacobian_scaling! end
+
+"""
+    is_inertia_correct(kkt::AbstractKKTSystem, n::Int, m::Int, p::Int)
+
+Check if the inertia ``(n, m, p)`` returned by the linear solver is adapted
+to the KKT system implemented in `kkt`.
+
+"""
+function is_inertia_correct end
+
+# TODO: temporary
+"Return true if KKT system is reduced."
+function is_reduced end
+
+"Nonzero in Jacobian"
+function nnz_jacobian end
+
+# TODO: we need these two templates as NLPModels does not implement
+# a template for dense Jacobian and dense Hessian
+"Dense Jacobian callback"
+function jac_dense! end
+
+"Dense Hessian callback"
+function hess_dense! end
+
+#=
+    Generic functions
+=#
+function initialize!(kkt::AbstractKKTSystem)
+    fill!(kkt.pr_diag, 1.0)
+    fill!(kkt.du_diag, 0.0)
+    fill!(kkt.hess, 0.0)
+end
+
+function regularize_diagonal!(kkt::AbstractKKTSystem, primal, dual)
+    kkt.pr_diag .+= primal
+    kkt.du_diag .= .-dual
+end
+
+Base.size(kkt::AbstractKKTSystem) = size(kkt.aug_com)
+Base.size(kkt::AbstractKKTSystem, dim::Int) = size(kkt.aug_com, dim)
+
+# Getters
+get_kkt(kkt::AbstractKKTSystem) = kkt.aug_com
+get_jacobian(kkt::AbstractKKTSystem) = kkt.jac
+get_hessian(kkt::AbstractKKTSystem) = kkt.hess
+get_raw_jacobian(kkt::AbstractKKTSystem) = kkt.jac_raw
+
+
+# Fix variable treatment
+function treat_fixed_variable!(kkt::AbstractKKTSystem{T, MT}) where {T, MT<:SparseMatrixCSC{T, Int32}}
+    length(kkt.ind_fixed) == 0 && return
+    aug = kkt.aug_com
+
+    fixed_aug_diag = view(aug.nzval, aug.colptr[kkt.ind_fixed])
+    fixed_aug_diag .= 1.0
+    fixed_aug = view(aug.nzval, kkt.ind_aug_fixed)
+    fixed_aug .= 0.0
+    return
+end
+function treat_fixed_variable!(kkt::AbstractKKTSystem{T, MT}) where {T, MT<:Matrix{T}}
+    length(kkt.ind_fixed) == 0 && return
+    aug = kkt.aug_com
+    @inbounds for i in kkt.ind_fixed
+        aug[i, :] .= 0.0
+        aug[:, i] .= 0.0
+        aug[i, i]  = 1.0
+    end
+end
+
+function is_inertia_correct(kkt::AbstractKKTSystem, num_pos, num_zero, num_neg)
+    return (num_zero == 0) && (num_pos == num_variables(kkt))
+end
+
+function build_kkt!(kkt::AbstractKKTSystem{T, MT}) where {T, MT<:Matrix{T}}
+    copyto!(kkt.aug_com, kkt.aug_raw)
+    treat_fixed_variable!(kkt)
+end
+
+function build_kkt!(kkt::AbstractKKTSystem{T, MT}) where {T, MT<:SparseMatrixCSC{T, Int32}}
+    transfer!(kkt.aug_com, kkt.aug_raw, kkt.aug_csc_map)
+    treat_fixed_variable!(kkt)
+end
+
+compress_hessian!(kkt::AbstractKKTSystem) = nothing
+
+
+include("sparse.jl")
+include("dense.jl")
+
---
+++ src/KKT/sparse.jl
@@ -0,0 +1,307 @@
+
+"""
+    SparseKKTSystem{T, MT} <: AbstractReducedKKTSystem{T, MT}
+
+Implement the [`AbstractReducedKKTSystem`](@ref) in sparse COO format.
+
+"""
+struct SparseKKTSystem{T, MT} <: AbstractReducedKKTSystem{T, MT}
+    hess::StrideOneVector{T}
+    jac::StrideOneVector{T}
+    pr_diag::StrideOneVector{T}
+    du_diag::StrideOneVector{T}
+    # Augmented system
+    aug_raw::SparseMatrixCOO{T,Int32}
+    aug_com::MT
+    aug_csc_map::Union{Nothing, Vector{Int}}
+    # Jacobian
+    jac_raw::SparseMatrixCOO{T,Int32}
+    jac_com::MT
+    jac_csc_map::Union{Nothing, Vector{Int}}
+    # Info
+    ind_ineq::Vector{Int}
+    ind_fixed::Vector{Int}
+    ind_aug_fixed::Vector{Int}
+    jacobian_scaling::Vector{T}
+end
+
+"""
+    SparseUnreducedKKTSystem{T, MT} <: AbstractUnreducedKKTSystem{T, MT}
+
+Implement the [`AbstractUnreducedKKTSystem`](@ref) in sparse COO format.
+
+"""
+struct SparseUnreducedKKTSystem{T, MT} <: AbstractUnreducedKKTSystem{T, MT}
+    hess::StrideOneVector{T}
+    jac::StrideOneVector{T}
+    pr_diag::StrideOneVector{T}
+    du_diag::StrideOneVector{T}
+
+    l_diag::StrideOneVector{T}
+    u_diag::StrideOneVector{T}
+    l_lower::StrideOneVector{T}
+    u_lower::StrideOneVector{T}
+
+    aug_raw::SparseMatrixCOO{T,Int32}
+    aug_com::MT
+    aug_csc_map::Union{Nothing, Vector{Int}}
+
+    jac_raw::SparseMatrixCOO{T,Int32}
+    jac_com::MT
+    jac_csc_map::Union{Nothing, Vector{Int}}
+    ind_ineq::Vector{Int}
+    ind_fixed::Vector{Int}
+    ind_aug_fixed::Vector{Int}
+    jacobian_scaling::Vector{T}
+end
+
+# Template to dispatch on sparse representation
+const AbstractSparseKKTSystem{T, MT} = Union{SparseKKTSystem{T, MT}, SparseUnreducedKKTSystem{T, MT}}
+
+#=
+    Generic sparse methods
+=#
+
+function mul!(y::AbstractVector, kkt::AbstractSparseKKTSystem, x::AbstractVector)
+    mul!(y, Symmetric(kkt.aug_com, :L), x)
+end
+
+function jtprod!(y::AbstractVector, kkt::AbstractSparseKKTSystem, x::AbstractVector)
+    mul!(y, kkt.jac_com', x)
+end
+
+nnz_jacobian(kkt::AbstractSparseKKTSystem) = nnz(kkt.jac_raw)
+
+function compress_jacobian!(kkt::AbstractSparseKKTSystem{T, MT}) where {T, MT<:SparseMatrixCSC{T, Int32}}
+    ns = length(kkt.ind_ineq)
+    kkt.jac[end-ns+1:end] .= -1.0
+    kkt.jac .*= kkt.jacobian_scaling # scaling
+    transfer!(kkt.jac_com, kkt.jac_raw, kkt.jac_csc_map)
+end
+
+function compress_jacobian!(kkt::AbstractSparseKKTSystem{T, MT}) where {T, MT<:Matrix{T}}
+    ns = length(kkt.ind_ineq)
+    kkt.jac[end-ns+1:end] .= -1.0
+    kkt.jac .*= kkt.jacobian_scaling # scaling
+    copyto!(kkt.jac_com, kkt.jac_raw)
+end
+
+function set_jacobian_scaling!(kkt::AbstractSparseKKTSystem{T, MT}, constraint_scaling::AbstractVector) where {T, MT}
+    nnzJ = length(kkt.jac)::Int
+    @inbounds for i in 1:nnzJ
+        index = kkt.jac_raw.I[i]
+        kkt.jacobian_scaling[i] = constraint_scaling[index]
+    end
+end
+
+
+#=
+    SparseKKTSystem
+=#
+
+function SparseKKTSystem{T, MT}(
+    n::Int, m::Int, ind_ineq::Vector{Int}, ind_fixed::Vector{Int},
+    hess_sparsity_I, hess_sparsity_J,
+    jac_sparsity_I, jac_sparsity_J,
+) where {T, MT}
+    n_jac = length(jac_sparsity_I)
+    n_hess = length(hess_sparsity_I)
+
+    aug_vec_length = n+m
+    aug_mat_length = n+m+n_hess+n_jac
+
+    I = Vector{Int32}(undef, aug_mat_length)
+    J = Vector{Int32}(undef, aug_mat_length)
+    V = Vector{T}(undef, aug_mat_length)
+    fill!(V, 0.0)  # Need to initiate V to avoid NaN
+
+    offset = n+n_jac+n_hess+m
+
+    I[1:n] .= 1:n
+    I[n+1:n+n_hess] = hess_sparsity_I
+    I[n+n_hess+1:n+n_hess+n_jac] .= (jac_sparsity_I.+n)
+    I[n+n_hess+n_jac+1:offset] .= (n+1:n+m)
+
+    J[1:n] .= 1:n
+    J[n+1:n+n_hess] = hess_sparsity_J
+    J[n+n_hess+1:n+n_hess+n_jac] .= jac_sparsity_J
+    J[n+n_hess+n_jac+1:offset] .= (n+1:n+m)
+
+    pr_diag = view(V, 1:n)
+    du_diag = view(V, n_jac+n_hess+n+1:n_jac+n_hess+n+m)
+
+    hess = view(V, n+1:n+n_hess)
+    jac = view(V, n_hess+n+1:n_hess+n+n_jac)
+
+    aug_raw = SparseMatrixCOO(aug_vec_length,aug_vec_length,I,J,V)
+    jac_raw = SparseMatrixCOO(m,n,jac_sparsity_I,jac_sparsity_J,jac)
+
+    aug_com = MT(aug_raw)
+    jac_com = MT(jac_raw)
+
+    aug_csc_map = get_mapping(aug_com, aug_raw)
+    jac_csc_map = get_mapping(jac_com, jac_raw)
+
+    ind_aug_fixed = if isa(aug_com, SparseMatrixCSC)
+        _get_fixed_variable_index(aug_com, ind_fixed)
+    else
+        zeros(Int, 0)
+    end
+    jac_scaling = ones(T, n_jac)
+
+    return SparseKKTSystem{T, MT}(
+        hess, jac, pr_diag, du_diag,
+        aug_raw, aug_com, aug_csc_map,
+        jac_raw, jac_com, jac_csc_map,
+        ind_ineq, ind_fixed, ind_aug_fixed, jac_scaling,
+    )
+end
+
+# Build KKT system directly from AbstractNLPModel
+function SparseKKTSystem{T, MT}(nlp::AbstractNLPModel, ind_cons=get_index_constraints(nlp)) where {T, MT}
+    n_slack = length(ind_cons.ind_ineq)
+    # Deduce KKT size.
+    n = get_nvar(nlp) + n_slack
+    m = get_ncon(nlp)
+    # Evaluate sparsity pattern
+    jac_I = Vector{Int32}(undef, get_nnzj(nlp))
+    jac_J = Vector{Int32}(undef, get_nnzj(nlp))
+    jac_structure!(nlp,jac_I, jac_J)
+
+    hess_I = Vector{Int32}(undef, get_nnzh(nlp))
+    hess_J = Vector{Int32}(undef, get_nnzh(nlp))
+    hess_structure!(nlp,hess_I,hess_J)
+
+    force_lower_triangular!(hess_I,hess_J)
+    # Incorporate slack's sparsity pattern
+    append!(jac_I, ind_cons.ind_ineq)
+    append!(jac_J, get_nvar(nlp)+1:get_nvar(nlp)+n_slack)
+
+    return SparseKKTSystem{T, MT}(
+        n, m, ind_cons.ind_ineq, ind_cons.ind_fixed,
+        hess_I, hess_J, jac_I, jac_J,
+    )
+end
+
+is_reduced(::SparseKKTSystem) = true
+num_variables(kkt::SparseKKTSystem) = length(kkt.pr_diag)
+
+
+#=
+    SparseUnreducedKKTSystem
+=#
+
+function SparseUnreducedKKTSystem{T, MT}(
+    n::Int, m::Int, nlb::Int, nub::Int, ind_ineq, ind_fixed,
+    hess_sparsity_I, hess_sparsity_J,
+    jac_sparsity_I, jac_sparsity_J,
+    ind_lb, ind_ub,
+) where {T, MT}
+    n_jac = length(jac_sparsity_I)
+    n_hess = length(hess_sparsity_I)
+
+    aug_mat_length = n + m + n_hess + n_jac + 2*nlb + 2*nub
+    aug_vec_length = n+m+nlb+nub
+
+    I = Vector{Int32}(undef, aug_mat_length)
+    J = Vector{Int32}(undef, aug_mat_length)
+    V = zeros(aug_mat_length)
+
+    offset = n+n_jac+n_hess+m
+
+    I[1:n] .= 1:n
+    I[n+1:n+n_hess] = hess_sparsity_I
+    I[n+n_hess+1:n+n_hess+n_jac].=(jac_sparsity_I.+n)
+    I[n+n_hess+n_jac+1:offset].=(n+1:n+m)
+
+    J[1:n] .= 1:n
+    J[n+1:n+n_hess] = hess_sparsity_J
+    J[n+n_hess+1:n+n_hess+n_jac].=jac_sparsity_J
+    J[n+n_hess+n_jac+1:offset].=(n+1:n+m)
+
+    I[offset+1:offset+nlb] .= (1:nlb).+(n+m)
+    I[offset+nlb+1:offset+2nlb] .= (1:nlb).+(n+m)
+    I[offset+2nlb+1:offset+2nlb+nub] .= (1:nub).+(n+m+nlb)
+    I[offset+2nlb+nub+1:offset+2nlb+2nub] .= (1:nub).+(n+m+nlb)
+    J[offset+1:offset+nlb] .= (1:nlb).+(n+m)
+    J[offset+nlb+1:offset+2nlb] .= ind_lb
+    J[offset+2nlb+1:offset+2nlb+nub] .= (1:nub).+(n+m+nlb)
+    J[offset+2nlb+nub+1:offset+2nlb+2nub] .= ind_ub
+
+    pr_diag = view(V,1:n)
+    du_diag = view(V,n_jac+n_hess+n+1:n_jac+n_hess+n+m)
+
+    l_diag = view(V,offset+1:offset+nlb)
+    l_lower= view(V,offset+nlb+1:offset+2nlb)
+    u_diag = view(V,offset+2nlb+1:offset+2nlb+nub)
+    u_lower= view(V,offset+2nlb+nub+1:offset+2nlb+2nub)
+
+    hess = view(V, n+1:n+n_hess)
+    jac = view(V, n_hess+n+1:n_hess+n+n_jac)
+
+    aug_raw = SparseMatrixCOO(aug_vec_length,aug_vec_length,I,J,V)
+    jac_raw = SparseMatrixCOO(m,n,jac_sparsity_I,jac_sparsity_J,jac)
+
+    aug_com = MT(aug_raw)
+    jac_com = MT(jac_raw)
+
+    aug_csc_map = get_mapping(aug_com, aug_raw)
+    jac_csc_map = get_mapping(jac_com, jac_raw)
+
+    jac_scaling = ones(T, n_jac)
+
+    ind_aug_fixed = if isa(aug_com, SparseMatrixCSC)
+        _get_fixed_variable_index(aug_com, ind_fixed)
+    else
+        zeros(Int, 0)
+    end
+
+    return SparseUnreducedKKTSystem{T, MT}(
+        hess, jac, pr_diag, du_diag,
+        l_diag, u_diag, l_lower, u_lower,
+        aug_raw, aug_com, aug_csc_map,
+        jac_raw, jac_com, jac_csc_map,
+        ind_ineq, ind_fixed, ind_aug_fixed, jac_scaling,
+    )
+end
+
+function SparseUnreducedKKTSystem{T, MT}(nlp::AbstractNLPModel, ind_cons=get_index_constraints(nlp)) where {T, MT}
+    n_slack = length(ind_cons.ind_ineq)
+    nlb = length(ind_cons.ind_lb)
+    nub = length(ind_cons.ind_ub)
+    # Deduce KKT size.
+    n = get_nvar(nlp) + n_slack
+    m = get_ncon(nlp)
+    # Evaluate sparsity pattern
+    jac_I = Vector{Int32}(undef, get_nnzj(nlp))
+    jac_J = Vector{Int32}(undef, get_nnzj(nlp))
+    jac_structure!(nlp,jac_I, jac_J)
+
+    hess_I = Vector{Int32}(undef, get_nnzh(nlp))
+    hess_J = Vector{Int32}(undef, get_nnzh(nlp))
+    hess_structure!(nlp,hess_I,hess_J)
+
+    force_lower_triangular!(hess_I,hess_J)
+    # Incorporate slack's sparsity pattern
+    append!(jac_I, ind_cons.ind_ineq)
+    append!(jac_J, get_nvar(nlp)+1:get_nvar(nlp)+n_slack)
+
+    return SparseUnreducedKKTSystem{T, MT}(
+        n, m, nlb, nub, ind_cons.ind_ineq, ind_cons.ind_fixed,
+        hess_I, hess_J, jac_I, jac_J, ind_cons.ind_lb, ind_cons.ind_ub,
+    )
+end
+
+function initialize!(kkt::SparseUnreducedKKTSystem)
+    kkt.pr_diag.=1
+    kkt.du_diag.=0
+    kkt.hess.=0
+    kkt.l_lower.=0
+    kkt.u_lower.=0
+    kkt.l_diag.=1
+    kkt.u_diag.=1
+end
+
+is_reduced(::SparseUnreducedKKTSystem) = false
+num_variables(kkt::SparseUnreducedKKTSystem) = length(kkt.pr_diag)
+
