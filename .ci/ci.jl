69b0bd389eeca4bfea72e3e097195e98640b2905
fpacaud <francoispacaud8@gmail.com>
2022-03-07 15:10:28 -0600

    debugging CI

--- .ci/ci.jl
+++ .ci/ci.jl
@@ -5,20 +5,21 @@ rm(joinpath(@__DIR__, "Manifest.toml");force=true)
 using Pkg
 
 println(@__DIR__)
+println(Pkg.status())
 Pkg.activate(@__DIR__)
 
-if ARGS[1] == "full"
-    pkgs = ["MadNLPHSL","MadNLPPardiso","MadNLPMumps","MadNLPGraph","MadNLPKrylov"]
-elseif ARGS[1] == "basic"
-    pkgs = ["MadNLPMumps","MadNLPGraph","MadNLPKrylov"]
-else
-    error("proper argument should be given - full or basic")
-end
+# if ARGS[1] == "full"
+#     pkgs = ["MadNLPHSL","MadNLPPardiso","MadNLPMumps","MadNLPGraph","MadNLPKrylov"]
+# elseif ARGS[1] == "basic"
+#     pkgs = ["MadNLPMumps","MadNLPGraph","MadNLPKrylov"]
+# else
+#     error("proper argument should be given - full or basic")
+# end
 
-Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..")))
-Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..","lib","MadNLPTests")))
-Pkg.develop.([PackageSpec(path=joinpath(@__DIR__,"..","lib",pkg)) for pkg in pkgs])
-Pkg.build()
+# Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..")))
+# Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..","lib","MadNLPTests")))
+# Pkg.develop.([PackageSpec(path=joinpath(@__DIR__,"..","lib",pkg)) for pkg in pkgs])
+# Pkg.build()
 
-Pkg.test.("MadNLP", coverage=true)
-Pkg.test.(pkgs, coverage=true)
+# Pkg.test.("MadNLP", coverage=true)
+# Pkg.test.(pkgs, coverage=true)
