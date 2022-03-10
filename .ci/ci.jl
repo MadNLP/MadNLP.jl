d1e3b33f7e49094c56ee496e327cba7f8bf86bde
fpacaud <francoispacaud8@gmail.com>
2022-03-10 16:38:23 -0600

    fix CI

--- .ci/ci.jl
+++ .ci/ci.jl
@@ -1,10 +1,11 @@
 
-rm(joinpath(@__DIR__, "Project.toml");force=true)
-rm(joinpath(@__DIR__, "Manifest.toml");force=true)
+rm(joinpath(@__DIR__, "Project.toml"); force=true)
+rm(joinpath(@__DIR__, "Manifest.toml"); force=true)
 
 using Pkg
-Pkg.gc()
 
+# Delete MathOptInterface directory to avoid side effect
+# with coverage files generated in a previous CI run.
 for _dep in Pkg.depots()
     moi_directory = joinpath(_dep, "packages", "MathOptInterface")
     if isdir(moi_directory)
@@ -15,18 +16,18 @@ end
 Pkg.activate(@__DIR__)
 
 
-# if ARGS[1] == "full"
-#     pkgs = ["MadNLPHSL","MadNLPPardiso","MadNLPMumps","MadNLPGPU","MadNLPKrylov"]
-# elseif ARGS[1] == "basic"
-#     pkgs = ["MadNLPMumps","MadNLPKrylov"]
-# else
-#     error("proper argument should be given - full or basic")
-# end
+if ARGS[1] == "full"
+    pkgs = ["MadNLPHSL","MadNLPPardiso","MadNLPMumps","MadNLPGPU","MadNLPKrylov"]
+elseif ARGS[1] == "basic"
+    pkgs = ["MadNLPMumps","MadNLPKrylov"]
+else
+    error("proper argument should be given - full or basic")
+end
 
-# Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..")))
-# Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..","lib","MadNLPTests")))
-# Pkg.develop.([PackageSpec(path=joinpath(@__DIR__,"..","lib",pkg)) for pkg in pkgs])
-# Pkg.build()
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..")))
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__,"..","lib","MadNLPTests")))
+Pkg.develop.([PackageSpec(path=joinpath(@__DIR__,"..","lib",pkg)) for pkg in pkgs])
+Pkg.build()
 
-# Pkg.test.("MadNLP", coverage=true)
-# Pkg.test.(pkgs, coverage=true)
+Pkg.test.("MadNLP", coverage=true)
+Pkg.test.(pkgs, coverage=true)
