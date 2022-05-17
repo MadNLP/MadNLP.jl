e3d00cffd075ed7c0529e890b1ab388983315947
fpacaud <francoispacaud8@gmail.com>
2022-05-17 14:38:09 -0500

    add documentation for LinearSolver's interface

--- src/LinearSolvers/linearsolvers.jl
+++ src/LinearSolvers/linearsolvers.jl
@@ -1,20 +1,86 @@
 # MadNLP.jl
 # Created by Sungho Shin (sungho.shin@wisc.edu)
 
+#=
+    LinearSolver's interface
+=#
+"""
+    AbstractLinearSolver
+
+Abstract type for linear solver targeting
+the resolution of the linear system ``Ax=b``.
+"""
 abstract type AbstractLinearSolver end
+
+"""
+    introduce(::AbstractLinearSolver)
+
+Print the name of the linear solver.
+"""
+function introduce end
+
+"""
+    factorize!(::AbstractLinearSolver)
+
+Factorize the matrix ``A`` and updates the factors
+inside the `AbstractLinearSolver` instance.
+"""
+function factorize! end
+
+"""
+    solve!(::AbstractLinearSolver, x)
+
+Solve the linear system ``Ax = b``.
+
+This function assumes the linear system has been
+factorized previously with [`factorize!`](@ref).
+"""
+function solve! end
+
+"""
+    is_inertia(::AbstractLinearSolver)
+
+Return `true` if the linear solver supports the
+computation of the inertia of the linear system.
+"""
+function is_inertia end
+
+"""
+    inertia(::AbstractLinearSolver)
+
+Return the inertia `(n, m, p)` of the linear system
+as a tuple.
+
+### Note
+The inertia is defined as a tuple ``(n, m, p)``,
+with
+- ``n``: number of positive eigenvalues
+- ``m``: number of negative eigenvalues
+- ``p``: number of zero eigenvalues
+"""
+function inertia end
+
+function improve! end
+
+#=
+    Iterator's interface
+=#
 abstract type AbstractIterator end
 
-# dummy solver type
-mutable struct EmptyLinearSolver <: AbstractLinearSolver end
+"""
+    solve_refine!(x, ::AbstractIterator, b)
 
-# dummy functions
-introduce(::EmptyLinearSolver) = ""
-factorize!(M::EmptyLinearSolver) = M
-solve!(::EmptyLinearSolver,x) = x
-is_inertia(::EmptyLinearSolver) = false
-inertia(::EmptyLinearSolver) = (0,0,0)
-improve!(::EmptyLinearSolver) = false
-solve_refine!(y,::AbstractIterator,x) = nothing
+Solve the linear system ``Ax = b`` using iterative
+refinement. The object `AbstractIterator` stores an instance
+of a [`AbstractLinearSolver`](@ref) for the backsolve
+operations.
+
+### Notes
+This function assumes the matrix stores in the linear solver
+has been factorized previously.
+
+"""
+function solve_refine! end
 
 # LinearSolverExceptions
 struct SymbolicException <: Exception end
