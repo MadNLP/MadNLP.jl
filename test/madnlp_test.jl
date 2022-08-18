e739a3b72285f6258a62c4fdef5a7d769c700432
François Pacaud <frapac@users.noreply.github.com>
2022-08-18 15:08:51 -0500

    add functions to decompose the time spent in callbacks/linear solver (#200)

--- test/madnlp_test.jl
+++ test/madnlp_test.jl
@@ -90,3 +90,15 @@ end
 #     @test ips.status == MadNLP.SOLVE_SUCCEEDED
 # end
 
+@testset "MadNLP timings" begin
+    nlp = MadNLPTests.HS15Model()
+    ips = MadNLP.InteriorPointSolver(nlp)
+    time_callbacks = MadNLP.timing_callbacks(ips)
+    @test isa(time_callbacks, NamedTuple)
+    time_linear_solver = MadNLP.timing_linear_solver(ips)
+    @test isa(time_linear_solver, NamedTuple)
+    time_madnlp = MadNLP.timing_madnlp(ips)
+    @test isa(time_madnlp.time_linear_solver, NamedTuple)
+    @test isa(time_madnlp.time_callbacks, NamedTuple)
+end
+
