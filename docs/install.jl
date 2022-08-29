91ef6d4006c23292532a203477226486f8d0a2b2
fpacaud <francoispacaud8@gmail.com>
2022-08-29 10:57:58 -0500

    fix generation of docs

--- docs/install.jl
+++ docs/install.jl
@@ -2,7 +2,7 @@ using Pkg
 
 MADNLP_DIR = pwd()
 
-Pkg.develop(PackageSpec(path=MADNLP_DIR))
+Pkg.develop(path=MADNLP_DIR)
 Pkg.develop(PackageSpec(path=joinpath(dirname(@__FILE__), "../lib/MadNLPTests/")))
 Pkg.instantiate()
 
