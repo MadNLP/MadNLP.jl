87a10c9a129bbc97f1092c15c457a55f4ff4e9ff
fpacaud <francoispacaud8@gmail.com>
2022-10-06 18:58:02 -0500

    address PR's comments

--- src/IPM/utils.jl
+++ src/IPM/utils.jl
@@ -36,15 +36,9 @@ getStatus(result::MadNLPExecutionStats) = STATUS_OUTPUT_DICT[result.status]
 # Utilities
 has_constraints(solver) = solver.m != 0
 
-# Print functions -----------------------------------------------------------
-function print_init(solver::AbstractMadNLPSolver)
-    @notice(solver.logger,@sprintf("Number of nonzeros in constraint Jacobian............: %8i", get_nnzj(solver.nlp.meta)))
-    @notice(solver.logger,@sprintf("Number of nonzeros in Lagrangian Hessian.............: %8i\n", get_nnzh(solver.nlp.meta)))
-
+function get_vars_info(solver)
     x_lb = get_lvar(solver.nlp)
     x_ub = get_uvar(solver.nlp)
-    g_lb = get_lcon(solver.nlp)
-    g_ub = get_ucon(solver.nlp)
     num_fixed = length(solver.ind_fixed)
     num_var = get_nvar(solver.nlp) - num_fixed
     num_llb_vars = length(solver.ind_llb)
@@ -56,6 +50,18 @@ function print_init(solver::AbstractMadNLPSolver)
         end
     end
     num_uub_vars = length(solver.ind_uub)
+    return (
+        n_free=num_var,
+        n_fixed=num_fixed,
+        n_only_lb=num_llb_vars,
+        n_only_ub=num_uub_vars,
+        n_bounded=num_lu_vars,
+    )
+end
+
+function get_cons_info(solver)
+    g_lb = get_lcon(solver.nlp)
+    g_ub = get_ucon(solver.nlp)
     # Classify constraints
     num_eq_cons, num_ineq_cons = 0, 0
     num_ue_cons, num_le_cons, num_lu_cons = 0, 0, 0
@@ -74,20 +80,35 @@ function print_init(solver::AbstractMadNLPSolver)
             end
         end
     end
+    return (
+        n_eq=num_eq_cons,
+        n_ineq=num_ineq_cons,
+        n_only_lb=num_le_cons,
+        n_only_ub=num_ue_cons,
+        n_bounded=num_lu_cons,
+    )
+end
 
-    if get_nvar(solver.nlp) < num_eq_cons
+# Print functions -----------------------------------------------------------
+function print_init(solver::AbstractMadNLPSolver)
+    @notice(solver.logger,@sprintf("Number of nonzeros in constraint Jacobian............: %8i", get_nnzj(solver.nlp.meta)))
+    @notice(solver.logger,@sprintf("Number of nonzeros in Lagrangian Hessian.............: %8i\n", get_nnzh(solver.nlp.meta)))
+    var_info = get_vars_info(solver)
+    con_info = get_cons_info(solver)
+
+    if get_nvar(solver.nlp) < con_info.n_eq
         throw(NotEnoughDegreesOfFreedomException())
     end
 
-    @notice(solver.logger,@sprintf("Total number of variables............................: %8i",num_var))
-    @notice(solver.logger,@sprintf("                     variables with only lower bounds: %8i",num_llb_vars))
-    @notice(solver.logger,@sprintf("                variables with lower and upper bounds: %8i",num_lu_vars))
-    @notice(solver.logger,@sprintf("                     variables with only upper bounds: %8i",num_uub_vars))
-    @notice(solver.logger,@sprintf("Total number of equality constraints.................: %8i",num_eq_cons))
-    @notice(solver.logger,@sprintf("Total number of inequality constraints...............: %8i",num_ineq_cons))
-    @notice(solver.logger,@sprintf("        inequality constraints with only lower bounds: %8i",num_le_cons))
-    @notice(solver.logger,@sprintf("   inequality constraints with lower and upper bounds: %8i",num_lu_cons))
-    @notice(solver.logger,@sprintf("        inequality constraints with only upper bounds: %8i\n",num_ue_cons))
+    @notice(solver.logger,@sprintf("Total number of variables............................: %8i",var_info.n_free))
+    @notice(solver.logger,@sprintf("                     variables with only lower bounds: %8i",var_info.n_only_lb))
+    @notice(solver.logger,@sprintf("                variables with lower and upper bounds: %8i",var_info.n_bounded))
+    @notice(solver.logger,@sprintf("                     variables with only upper bounds: %8i",var_info.n_only_ub))
+    @notice(solver.logger,@sprintf("Total number of equality constraints.................: %8i",con_info.n_eq))
+    @notice(solver.logger,@sprintf("Total number of inequality constraints...............: %8i",con_info.n_ineq))
+    @notice(solver.logger,@sprintf("        inequality constraints with only lower bounds: %8i",con_info.n_only_lb))
+    @notice(solver.logger,@sprintf("   inequality constraints with lower and upper bounds: %8i",con_info.n_bounded))
+    @notice(solver.logger,@sprintf("        inequality constraints with only upper bounds: %8i\n",con_info.n_only_ub))
     return
 end
 
