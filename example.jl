5a0caa0a6df36eb28dc80ffa2759a72e06295ecf
Sungho Shin <sshin@anl.gov>
2023-06-10 22:08:13 -0500

    playing with parameters

--- example.jl
+++ example.jl
@@ -1,24 +1,31 @@
 using MadNLP, MadNLPHSL, MadNLPCUSOLVER, SIMDiffExamples; SIMDiffExamples.silence()
 
-tol = 1e-1
+tol = 1e-3
 
 case = 
     # "/home/sshin/git/pglib-opf/pglib_opf_case30_ieee.m"
     # "/home/sshin/git/pglib-opf/pglib_opf_case300_ieee.m"
-    # "/home/sshin/git/pglib-opf/pglib_opf_case1354_pegase.m"
-    "/home/sshin/git/pglib-opf/pglib_opf_case2869_pegase.m"
+    "/home/sshin/git/pglib-opf/pglib_opf_case1354_pegase.m"
+    # "/home/sshin/git/pglib-opf/pglib_opf_case2869_pegase.m"
     # "/home/sshin/git/pglib-opf/pglib_opf_case9241_pegase.m"
-    # "/home/sshin/git/pglib-opf/pglib_opf_case2000_goc.m"
+    # "/home/sshin/git/pglib-opf/pglib_opf_case13659_pegase.m"
+    # "/home/sshin/git/PowerSystemsTestData/ACTIVSg70k/case_ACTIVSg70k.m"
 
 m = SIMDiffExamples.ac_power_model(case)
-
-# m = SIMDiffExamples.luksan_vlcek_model(10)
-m.meta.ucon .+= tol
-
-s1 = MadNLPSolver(
+s2 = MadNLPSolver(
     m;
     linear_solver=Ma27Solver,
-    # linear_solver=MadNLPCUSOLVER.RFSolver,
+    tol = tol,
+    # kkt_system=MadNLP.SPARSE_CONDENSED_KKT_SYSTEM,
+    # fixed_variable_treatment = MadNLP.RELAX_BOUND
+)
+@time MadNLP.solve!(s2)
+
+m.meta.ucon .+= tol
+s1 = MadNLPSolver(
+    m;
+    # linear_solver=Ma27Solver,
+    linear_solver=MadNLPCUSOLVER.RFSolver,
     tol = tol,
     kkt_system=MadNLP.SPARSE_CONDENSED_KKT_SYSTEM,
     fixed_variable_treatment = MadNLP.RELAX_BOUND
@@ -26,11 +33,3 @@ s1 = MadNLPSolver(
 @time MadNLP.solve!(s1)
 
 
-# s2 = MadNLPSolver(
-#     m;
-#     linear_solver=Ma27Solver,
-#     tol = tol,
-#     # kkt_system=MadNLP.SPARSE_CONDENSED_KKT_SYSTEM,
-#     # fixed_variable_treatment = MadNLP.RELAX_BOUND
-# )
-# @time MadNLP.solve!(s2)
