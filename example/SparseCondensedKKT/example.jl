8738fcb43cd04a8df6abc3085380c1d3b0fbd084
Sungho Shin <sshin@anl.gov>
2023-07-05 21:04:44 -0500

    improving example.jl

--- example/SparseCondensedKKT/example.jl
+++ example/SparseCondensedKKT/example.jl
@@ -1,5 +1,7 @@
 using MadNLP, MadNLPHSL, MadNLPGPU, SIMDiffExamples, CUDA
 using JuMP, Ipopt
+using StatsPlots, LaTeXStrings
+pgfplotsx()
 
 SIMDiffExamples.silence()
 CUDA.allowscalar(false)
@@ -8,9 +10,15 @@ tol=1e-5
 save = []
 
 for (path,casename) in (
-    (ENV["PGLIB_PATH"], "pglib_opf_case9241_pegase.m"),
-    (ENV["PGLIB_PATH"], "pglib_opf_case13659_pegase.m"),
-    ("/home/sshin/git/PowerSystemsTestData/ACTIVSg70k/", "case_ACTIVSg70k.m")
+    (ENV["PGLIB_PATH"], "pglib_opf_case89_pegase.m"),
+    (ENV["PGLIB_PATH"], "pglib_opf_case1354_pegase.m"),
+    # (ENV["PGLIB_PATH"], "pglib_opf_case2869_pegase.m"),
+    # (ENV["PGLIB_PATH"], "pglib_opf_case8387_pegase.m"),
+    # (ENV["PGLIB_PATH"], "pglib_opf_case9241_pegase.m"),
+    # (ENV["PGLIB_PATH"], "pglib_opf_case13659_pegase.m"),
+    # ("/home/sshin/git/PowerSystemsTestData/ACTIVSg2000/", "case_ACTIVSg2000.m"),
+    # ("/home/sshin/git/PowerSystemsTestData/ACTIVSg10k/", "case_ACTIVSg10k.m"),
+    # ("/home/sshin/git/PowerSystemsTestData/ACTIVSg70k/", "case_ACTIVSg70k.m"),
     )
 
     case = joinpath(path, casename)
@@ -21,12 +29,15 @@ for (path,casename) in (
     m.meta.ucon .+= tol
 
     t11 = @elapsed begin
-        s1=MadNLPSolver(m; kkt_system=MadNLP.SparseCondensedKKTSystem, linear_solver=MadNLPGPU.RFSolver, tol=tol)
-        MadNLP.initialize!(s1.kkt)
-        solve!(s1)
+        madnlp(m; kkt_system=MadNLP.SparseCondensedKKTSystem, linear_solver=MadNLPGPU.RFSolver, tol=tol)
     end
     t21 = sum(getfield(m.inner.counters, t) for t in [:teval_obj, :teval_obj, :teval_obj, :teval_obj, :teval_obj])
 
+    m = MadNLP.ScaledNLPModel(SIMDiffExamples.ac_power_model(case))
+    t11 = @elapsed begin
+        madnlp(m; kkt_system=MadNLP.SparseCondensedKKTSystem, linear_solver=MadNLPGPU.RFSolver, tol=tol)
+    end
+    t21 = sum(getfield(m.inner.counters, t) for t in [:teval_obj, :teval_obj, :teval_obj, :teval_obj, :teval_obj])
 
     m = SIMDiffExamples.jump_ac_power_model(case)
     set_optimizer(m, Ipopt.Optimizer)
@@ -41,15 +52,10 @@ for (path,casename) in (
 end
 
 
-using StatsPlots, LaTeXStrings
-pgfplotsx()
 
-stime = vcat([[t11-t21, t12-t22] for (casename, t11, t12, t21, t22) in save]...)
-ftime = vcat([[t21, t22] for (casename, t11, t12, t21, t22) in save]...)
-ticks = ["case 9241 (GPU)", "case 9241 (CPU)", "", "case 13659 (GPU)", "case 13659 (CPU)", "", "case 70k (GPU)", "case 70k (CPU)"]
-
-stime = [stime[1:2]; NaN; stime[3:4]; NaN; stime[5:6]]
-ftime = [ftime[1:2]; NaN; ftime[3:4]; NaN; ftime[5:6]]
+stime = vcat([[t11-t21, t13-t23] for (casename, t11, t12, t21, t22) in save]...)
+ftime = vcat([[t21, t23] for (casename, t11, t12, t21, t22) in save]...)
+ticks = vcat([["SIMDiff + MadNLP (GPU)", "JuMP + Ipopt (CPU)"] for (casename, t11, t12, t21, t22) in save]...)
 
 plt = groupedbar(
     [stime ftime],
@@ -59,11 +65,10 @@ plt = groupedbar(
     framestyle=:box,
     label=["Solver Internal + Linear Algebra" "Derivative Evaluation"],
     ylim=(0,1.1*maximum(stime.+ftime)),
-    ylabel="Wall Time (sec)",
+    ylabel="Wall Clock Time (sec)",
     xrotation = 60,
     legend=:topleft,
     size=(600,300),
 )
-
 
 savefig(plt, "bar.pdf")
