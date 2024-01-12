1d721a92fba19d59893db3e8cd0aa707fd5d146c
Sungho Shin <sshin@anl.gov>
2024-01-11 21:15:43 -0600

    fixup! Merge remote-tracking branch 'origin/master' into fp/options

--- src/IPM/IPM.jl
+++ src/IPM/IPM.jl
@@ -148,7 +148,7 @@ function MadNLPSolver(nlp::AbstractNLPModel{T,VT}; kwargs...) where {T, VT}
         ind_cons,
         ipm_opt.linear_solver;
         hessian_approximation=ipm_opt.hessian_approximation,
-        opt_linear_solver=opt_linear_solver,
+        opt_linear_solver=options.linear_solver,
     )
 
     @trace(logger,"Initializing iterative solver.")
