485163f5143ec1cc3c30c9ff7979696dc80eb7b8
Sungho Shin <sshin@anl.gov>
2024-03-01 16:39:57 -0600

    cudss test changed

--- lib/MadNLPGPU/test/madnlpgpu_test.jl
+++ lib/MadNLPGPU/test/madnlpgpu_test.jl
@@ -3,6 +3,14 @@ testset = [
     [
         "LapackGPU-CUSOLVERRF",
         ()->MadNLP.Optimizer(
+            linear_solver=MadNLPGPU.CUDSSSolver,
+            print_level=MadNLP.ERROR
+        ),
+        [],
+    ],
+    [
+        "LapackGPU-CUSOLVERRF",
+        ()->MadNLP.Optimizer(
             linear_solver=MadNLPGPU.RFSolver,
             print_level=MadNLP.ERROR
         ),
@@ -68,30 +76,5 @@ testset = [
     # Test LapackGPU wrapper
     for (name,optimizer_constructor,exclude) in testset
         test_madnlp(name,optimizer_constructor,exclude; Arr= CuArray)
-    end
-end
-
-@testset "CUDSS Test" begin
-    @testset "Interface" for T in [Float32, Float64]
-        MadNLPTests.test_linear_solver(MadNLPGPU.CUDSSSolver, T)
-    end
-    @testset "HS15 Test" begin
-        nlp = MadNLPTests.HS15Model()
-        solver = MadNLP.MadNLPSolver(
-            nlp;
-            linear_solver=MadNLPGPU.CUDSSSolver,
-            print_level=MadNLP.ERROR,
-            kkt_system=MadNLP.SparseKKTSystem,
-            callback=MadNLP.SparseCallback,
-        )
-        MadNLP.solve!(solver)
-        @test solver.status == MadNLP.SOLVE_SUCCEEDED
-    end
-    @testset "MadNLP Test" begin
-        constructor = () -> MadNLP.Optimizer(
-            linear_solver=MadNLPGPU.CUDSSSolver,
-            print_level=MadNLP.ERROR,
-        )
-        test_madnlp("cuDSS", constructor, [])
     end
 end
