41062e4b074c7c9941dc6308d2b4aa8631d5c11e
Sungho Shin <sshin@anl.gov>
2024-03-07 12:41:11 -0600

    MOI interface moved to ext (#268)
    * MadNLPMOI extension added
    
    * MadNLP version bug fix
    
    * renamed ext.jl file
    
    * export bug fix
    
    * typo fix

--- src/MadNLP.jl
+++ src/MadNLP.jl
@@ -1,8 +1,6 @@
 module MadNLP
 
 import Pkg.TOML: parsefile
-import MathOptInterface
-import Libdl: dlopen, dlext, RTLD_DEEPBIND, RTLD_GLOBAL
 import Printf: @sprintf
 import LinearAlgebra: BLAS, Adjoint, Symmetric, mul!, ldiv!, norm, dot, diagind, normInf, transpose!, issuccess
 import LinearAlgebra: cholesky, qr, lu, cholesky!, axpy!
@@ -11,12 +9,8 @@ import SparseArrays: SparseArrays, AbstractSparseMatrix, SparseMatrixCSC, sparse
 import Base: string, show, print, size, getindex, copyto!, @kwdef
 import SuiteSparse: UMFPACK, CHOLMOD
 import NLPModels
-import NLPModels: finalize, AbstractNLPModel, obj, grad!, cons!, jac_coord!, hess_coord!, hess_structure!, jac_structure!, NLPModelMeta, get_nvar, get_ncon, get_minimize, get_x0, get_y0, get_nnzj, get_nnzh, get_lvar, get_uvar, get_lcon, get_ucon, Counters as _Counters # get_zl,get_zu
+import NLPModels: finalize, AbstractNLPModel, obj, grad!, cons!, jac_coord!, hess_coord!, hess_structure!, jac_structure!, NLPModelMeta, get_nvar, get_ncon, get_minimize, get_x0, get_y0, get_nnzj, get_nnzh, get_lvar, get_uvar, get_lcon, get_ucon
 import SolverCore: solve!, getStatus, AbstractOptimizationSolver, AbstractExecutionStats
-
-const MOI = MathOptInterface
-const MOIU = MathOptInterface.Utilities
-
 export MadNLPSolver, MadNLPOptions, UmfpackSolver, LDLSolver, CHOLMODSolver, LapackCPUSolver, madnlp, solve!
 
 # Version info
@@ -32,6 +26,6 @@ include(joinpath("KKT", "KKTsystem.jl"))
 include(joinpath("LinearSolvers","linearsolvers.jl"))
 include("options.jl")
 include(joinpath("IPM", "IPM.jl"))
-include(joinpath("Interfaces","interfaces.jl"))
+include("extension_templates.jl")
 
 end # end module
