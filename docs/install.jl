64469bf3a375cf0ba727ca11935de9a0ab074c53
Sungho Shin <sshin@anl.gov>
2024-03-07 22:29:28 -0600

    docs ci fixed (#307)

--- docs/install.jl
+++ docs/install.jl
@@ -2,7 +2,7 @@ using Pkg
 
 MADNLP_DIR = pwd()
 
+Pkg.develop(path=joinpath(MADNLP_DIR, "lib", "MadNLPTests"))
 Pkg.develop(path=MADNLP_DIR)
-Pkg.develop(PackageSpec(path=joinpath(dirname(@__FILE__), "../lib/MadNLPTests/")))
 Pkg.instantiate()
 
