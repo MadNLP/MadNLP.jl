6fb64eb51bf1ef1a4d718fc61f88ef8d5e153bb1
Sungho Shin <sshin@anl.gov>
2024-04-12 09:49:52 -0500

    changed everything to @NL

--- lib/MadNLPTests/src/MadNLPTests.jl
+++ lib/MadNLPTests/src/MadNLPTests.jl
@@ -337,9 +337,9 @@ function lp(optimizer_constructor::Function; Arr = Array)
         model = Model()
         @variable(model, x >= 0)
         @variable(model, 0 <= y <= 3)
-        @objective(model, Min, 12x + 20y)
-        @constraint(model, c1, 6x + 8y >= 100)
-        @constraint(model, c2, 7x + 12y >= 120)
+        @NLobjective(model, Min, 12x + 20y)
+        @NLconstraint(model, c1, 6x + 8y >= 100)
+        @NLconstraint(model, c2, 7x + 12y >= 120)
 
         nlp = SparseWrapperModel(
             Arr,
