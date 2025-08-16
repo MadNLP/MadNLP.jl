1ce6c2ea59b93c00890acbea64950bb54bf358ef
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-16 01:45:59 -0500

    Remove the test with CUDA.functional()

--- lib/MadNLPGPU/test/runtests.jl
+++ lib/MadNLPGPU/test/runtests.jl
@@ -1,8 +1,6 @@
 using Test, CUDA, MadNLP, MadNLPGPU, MadNLPTests
 
 @testset "MadNLPGPU test" begin
-    if CUDA.functional()
-        include("madnlpgpu_test.jl")
-        include("densekkt_gpu.jl")
-    end
+    include("madnlpgpu_test.jl")
+    include("densekkt_gpu.jl")
 end
