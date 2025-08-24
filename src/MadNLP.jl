0c2741cde48e24af5e1a0793ab28f77396e9708a
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-24 15:05:56 -0500

    [MadNLP] Update the interface for LapackCPUSolver

--- src/MadNLP.jl
+++ src/MadNLP.jl
@@ -4,7 +4,7 @@ import Pkg.TOML: parsefile
 import Printf: @sprintf
 import LinearAlgebra: BLAS, Adjoint, Symmetric, mul!, ldiv!, norm, dot, diagind, normInf, transpose!, issuccess
 import LinearAlgebra: cholesky, qr, lu, cholesky!, axpy!
-import LinearAlgebra.BLAS: symv!, ger!, libblas, liblapack, BlasInt, @blasfunc
+import LinearAlgebra.BLAS: symv!, ger!, libblastrampoline, BlasInt, @blasfunc
 import SparseArrays: SparseArrays, AbstractSparseMatrix, SparseMatrixCSC, sparse, getcolptr, rowvals, nnz, nonzeros
 import Base: string, show, print, size, getindex, copyto!, @kwdef
 import SuiteSparse: UMFPACK, CHOLMOD
