5a8929eb1d44019be1e26fe0676e4001e1853b94
Sungho Shin <sushin@mit.edu>
2025-10-20 11:23:56 -0400

    some more ext updates

--- ext/MadNLPJuMPExt.jl
+++ ext/MadNLPJuMPExt.jl
@@ -1,18 +1,12 @@
 module MadNLPJuMPExt
 
-using MadNLP, MathOptInterface, JuMP, NLPModels
+using MadNLP, MathOptInterface, JuMP, NLPModels, PrecompileTools
 
-MadNLP.@setup_workload begin
+PrecompileTools.@setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
 
-    MadNLP.@compile_workload begin
-        m = Model(MadNLP.Optimizer)
-        @variable(m, x)
-        @constraint(m, exp(x) >= 0)
-        @objective(m, Min, (x-1)^4)
-        optimize!(m)
-        
+    PrecompileTools.@compile_workload begin
         nlp = MadNLP.HS15Model()
         MadNLP.madnlp(nlp; print_level=MadNLP.ERROR)
         precompile(Tuple{typeof(MadNLP.madnlp), NLPModels.AbstractNLPModel{T, S} where S where T})
