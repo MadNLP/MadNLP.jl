61b70e3ca3dd3adff95d9865115884ff5fbc656a
Alexis Montoison <alexis.montoison@polymtl.ca>
2026-01-07 00:21:18 +0100

    Finaliza fix

--- src/IPM/callbacks.jl
+++ src/IPM/callbacks.jl
@@ -74,7 +74,27 @@ function eval_jac_wrapper!(solver::AbstractMadNLPSolver, kkt::AbstractKKTSystem,
     return jac
 end
 
-function eval_lag_hess_wrapper!(solver::AbstractMadNLPSolver, kkt::AbstractKKTSystem, x::PrimalVector{T},l::AbstractVector{T};is_resto=false) where T
+function eval_lag_hess_wrapper!(solver::AbstractMadNLPSolver, kkt::AbstractKKTSystem, x::PrimalVector{T}; is_resto=false) where T
+    nlp = solver.nlp
+    cnt = solver.cnt
+    @trace(solver.logger,"Evaluating Lagrangian Hessian.")
+    hess = get_hessian(kkt)
+    scale = (get_minimize(nlp) ? one(T) : -one(T)) * (is_resto ? zero(T) : one(T))
+    cnt.eval_function_time += @elapsed _eval_lag_hess_wrapper!(
+        solver.cb,
+        variable(x),
+        hess;
+        obj_weight = scale,
+    )
+    compress_hessian!(kkt)
+    cnt.lag_hess_cnt += 1
+    if cnt.lag_hess_cnt == 1 && !is_valid(hess)
+        throw(InvalidNumberException(:hess))
+    end
+    return hess
+end
+
+function eval_lag_hess_wrapper!(solver::AbstractMadNLPSolver, kkt::AbstractKKTSystem, x::PrimalVector{T}, l::AbstractVector{T}; is_resto=false) where T
     nlp = solver.nlp
     cnt = solver.cnt
     @trace(solver.logger,"Evaluating Lagrangian Hessian.")
@@ -118,6 +138,31 @@ end
 function eval_lag_hess_wrapper!(
     solver::AbstractMadNLPSolver,
     kkt::AbstractDenseKKTSystem{T, VT, MT, QN},
+    x::PrimalVector{T};
+    is_resto=false,
+) where {T, VT, MT, QN<:ExactHessian}
+    nlp = solver.nlp
+    cnt = solver.cnt
+    @trace(solver.logger,"Evaluating Lagrangian Hessian.")
+    hess = get_hessian(kkt)
+    scale = is_resto ? zero(T) : get_minimize(nlp) ? one(T) : -one(T)
+    cnt.eval_function_time += @elapsed _eval_lag_hess_wrapper!(
+        solver.cb,
+        variable(x),
+        hess;
+        obj_weight = scale,
+    )
+    compress_hessian!(kkt)
+    cnt.lag_hess_cnt+=1
+    if cnt.lag_hess_cnt == 1 && !is_valid(hess)
+        throw(InvalidNumberException(:hess))
+    end
+    return hess
+end
+
+function eval_lag_hess_wrapper!(
+    solver::AbstractMadNLPSolver,
+    kkt::AbstractDenseKKTSystem{T, VT, MT, QN},
     x::PrimalVector{T},
     l::AbstractVector{T};
     is_resto=false,
@@ -140,6 +185,45 @@ function eval_lag_hess_wrapper!(
         throw(InvalidNumberException(:hess))
     end
     return hess
+end
+
+function eval_lag_hess_wrapper!(
+    solver::AbstractMadNLPSolver,
+    kkt::AbstractKKTSystem{T, VT, MT, QN},
+    x::PrimalVector{T};
+    is_resto=false,
+) where {T, VT, MT<:AbstractMatrix{T}, QN<:AbstractQuasiNewton{T, VT}}
+    cb = solver.cb
+    cnt = solver.cnt
+    @trace(solver.logger, "Update BFGS matrices.")
+
+    qn = kkt.quasi_newton
+    Bk = kkt.hess
+    sk, yk = qn.sk, qn.yk
+    n = length(qn.sk)
+
+    if cnt.obj_grad_cnt >= 2
+        # Build sk = x+ - x
+        copyto!(sk, 1, variable(solver.x), 1, n)   # sₖ = x₊
+        axpy!(-one(T), qn.last_x, sk)              # sₖ = x₊ - x
+        # Build yk = ∇L+ - ∇L
+        copyto!(yk, 1, variable(solver.f), 1, n)   # yₖ = ∇f₊
+        axpy!(-one(T), qn.last_g, yk)              # yₖ = ∇f₊ - ∇f
+        # Update quasi-Newton approximation.
+        update!(qn, Bk, sk, yk)
+    else
+        # Init quasi-Newton approximation
+        g0 = variable(solver.f)
+        f0 = solver.obj_val
+        init!(qn, Bk, g0, f0)
+    end
+
+    # Backup data for next step
+    copyto!(qn.last_x, 1, variable(solver.x), 1, n)
+    copyto!(qn.last_g, 1, variable(solver.f), 1, n)
+
+    compress_hessian!(kkt)
+    return get_hessian(kkt)
 end
 
 function eval_lag_hess_wrapper!(
@@ -188,4 +272,3 @@ function eval_lag_hess_wrapper!(
     compress_hessian!(kkt)
     return get_hessian(kkt)
 end
-
