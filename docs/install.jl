f9ff711a923f2a5f9b3adf5f2d08490bc55b7189
fpacaud <francoispacaud8@gmail.com>
2026-01-22 18:36:51 +0100

    try fixing cache for Documenter

--- docs/install.jl
+++ docs/install.jl
@@ -5,5 +5,7 @@ MADNLP_DIR = pwd()
 Pkg.develop(path=joinpath(MADNLP_DIR, "lib", "MadNLPTests"))
 Pkg.develop(path=joinpath(MADNLP_DIR, "lib", "MadNLPGPU"))
 Pkg.develop(path=MADNLP_DIR)
+# Compile cache for Documenter to avoid issue with MbedTLS
+Base.compilecache(Base.identify_package("Documenter"))
 Pkg.instantiate()
 
