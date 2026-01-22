de1e2a905ad9d690edfa5de4b17f23d4e18c7b66
fpacaud <francoispacaud8@gmail.com>
2026-01-22 17:56:51 +0100

    Fix generation of documentation

--- docs/install.jl
+++ docs/install.jl
@@ -3,7 +3,7 @@ using Pkg
 MADNLP_DIR = pwd()
 
 Pkg.develop(path=joinpath(MADNLP_DIR, "lib", "MadNLPTests"))
-Pkg.develop(path="https://github.com/MadNLP/HybridKKT.jl")
+Pkg.develop(path=joinpath(MADNLP_DIR, "lib", "MadNLPGPU"))
 Pkg.develop(path=MADNLP_DIR)
 Pkg.instantiate()
 
