f6dcbf9b0721937a640eeecfce3fb4fff1ccb093
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-02-11 08:47:28 -0500

    Gate more things behind Base.USE_GPL_LIBS

--- src/MadNLP.jl
+++ src/MadNLP.jl
@@ -7,11 +7,12 @@ import LinearAlgebra: cholesky, qr, lu, cholesky!, axpy!
 import LinearAlgebra.BLAS: symv!, ger!, libblastrampoline, BlasInt, @blasfunc
 import SparseArrays: SparseArrays, AbstractSparseMatrix, SparseMatrixCSC, sparse, getcolptr, rowvals, nnz, nonzeros
 import Base: string, show, print, size, getindex, copyto!, @kwdef
-import SuiteSparse: UMFPACK, CHOLMOD
+Base.USE_GPL_LIBS && import SuiteSparse: UMFPACK, CHOLMOD
 import NLPModels
 import NLPModels: finalize, AbstractNLPModel, obj, grad!, cons!, jac_coord!, hess_coord!, hess_structure!, jac_structure!, NLPModelMeta, get_nvar, get_ncon, get_minimize, get_x0, get_y0, get_nnzj, get_nnzh, get_lvar, get_uvar, get_lcon, get_ucon
 import SolverCore: solve!, getStatus, AbstractOptimizationSolver, AbstractExecutionStats
-export MadNLPSolver, MadNLPOptions, UmfpackSolver, LDLSolver, CHOLMODSolver, LapackCPUSolver, madnlp, solve!
+export MadNLPSolver, MadNLPOptions, LDLSolver, LapackCPUSolver, madnlp, solve!
+Base.USE_GPL_LIBS && export UmfpackSolver, CHOLMODSolver
 import LDLFactorizations
 
 # Version info
