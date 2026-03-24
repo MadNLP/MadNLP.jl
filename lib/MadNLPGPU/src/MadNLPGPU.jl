0eec98b05384886e326be4acf2cbf4d2099d3c13
Anton Pozharskiy <apozharski@gmail.com>
2026-03-24 09:44:22 +0100

    mapreduce for AbstractGPUVector

--- lib/MadNLPGPU/src/MadNLPGPU.jl
+++ lib/MadNLPGPU/src/MadNLPGPU.jl
@@ -21,7 +21,11 @@ import MadNLP:
     setup_cholesky!, setup_lu!, setup_qr!, setup_evd!, setup_bunchkaufman!,
     factorize_cholesky!, factorize_lu!, factorize_qr!, factorize_evd!, factorize_bunchkaufman!,
     solve_cholesky!, solve_lu!, solve_qr!, solve_evd!, solve_bunchkaufman!
-
+import MadNLP:
+    _get_varphi, get_varphi, get_inf_du, get_inf_compl, get_min_complementarity,
+    get_varphi_d, get_alpha_max, get_alpha_z, get_obj_val_R, get_theta_R, get_inf_pr_R,
+    get_inf_du_R, get_inf_compl_R, get_alpha_max_R, get_alpha_z_R, get_varphi_R, get_F,
+    get_varphi_d_R, get_rel_search_norm
 # AMD and Metis
 import AMD, Metis
 
