bede109443e74aaee63221ff7aa96caa40b43ee4
Michel Schanen <Michel.Schanen@gmail.com>
2026-05-01 08:49:03 -0500

    Remove two-stage tutorial until ExaModels 0.10 release

--- docs/make.jl
+++ docs/make.jl
@@ -24,7 +24,6 @@ makedocs(
             "Warm-start" => "tutorials/warmstart.md",
             "Quasi-Newton" => "tutorials/quasi_newton.md",
             "Custom KKT system" => "tutorials/kktsystem.md",
-            "Schur complement KKT" => "tutorials/schur.md",
         ],
         "Manual" => [
             "KKT systems" => "man/kkt.md",
