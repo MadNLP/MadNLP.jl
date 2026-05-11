c181c11c332cd0062f87cb39a40f5256720fe3a2
Michel Schanen <Michel.Schanen@gmail.com>
2026-05-11 11:47:26 -0500

    Julia 1.12 global var regression

--- lib/MadNLPGPU/test/densekkt_rocm.jl
+++ lib/MadNLPGPU/test/densekkt_rocm.jl
@@ -9,7 +9,7 @@ function _compare_amdgpu_with_cpu(KKTSystem, n, m, ind_fixed)
         madnlp_options = Dict{Symbol, Any}(
             :callback=>MadNLP.DenseCallback,
             :kkt_system=>KKTSystem,
-            :linear_solver=>LapackROCmSolver,
+            :linear_solver=>MadNLPGPU.LapackROCmSolver,
             :lapack_algorithm=>MadNLP.QR,
             :print_level=>MadNLP.ERROR,
             :tol=>tol
@@ -65,7 +65,7 @@ end
             callback=MadNLP.DenseCallback,
             print_level=MadNLP.ERROR,
             kkt_system=KKT,
-            linear_solver=LapackROCmSolver,
+            linear_solver=MadNLPGPU.LapackROCmSolver,
         )
         results_ref = MadNLP.solve!(solver_exact)
 
@@ -76,7 +76,7 @@ end
             print_level=MadNLP.ERROR,
             kkt_system=KKT,
             hessian_approximation=QN,
-            linear_solver=LapackROCmSolver,
+            linear_solver=MadNLPGPU.LapackROCmSolver,
         )
         results_qn = MadNLP.solve!(solver_qn)
 
